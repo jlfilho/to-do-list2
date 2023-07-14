3aec19f529b7a9acb91a4a3b4c292ccb0d06f376
João da Mata <jlfilho@uea.edu.br>
2023-07-14 18:46:52 -0400

    Verson 0.0.1: Componentes criados

---
+++ src/app/modules/home/components/hearder/hearder.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HearderComponent } from './hearder.component';
+
+describe('HearderComponent', () => {
+  let component: HearderComponent;
+  let fixture: ComponentFixture<HearderComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HearderComponent]
+    });
+    fixture = TestBed.createComponent(HearderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
